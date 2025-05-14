85a4706637986e7360188a66de400bca87dab7a5
Manuel Rodriguez <cuentapersonal.0098@gmail.com>
2025-05-14 11:54:36 -0500

    .\src\components\show-projects.jsx: Modificacion, ya se obtiene los proyectos desde un archivo local

--- src/components/show-projects.jsx
+++ src/components/show-projects.jsx
@@ -1,9 +1,19 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router';
 import '../assets/style/show-projects.css';
+import { projects } from '../data/projects';
+
 
 
 export const ShowProjects = ()=>{
+
+    const [ projectsState, setProjectsState ] = useState([]);
+    
+    useEffect(()=>{ 
+    
+        projects && setProjectsState( projects );
+
+    },[]);
+    
 
     const changeClass = ( { target } )=>{
         
@@ -18,68 +28,36 @@ export const ShowProjects = ()=>{
             selectProject.classList.add( 'show-active' );
 
             return
-
         };
 
         selectProject.classList.remove( 'show-active' );
-
     };
 
+    return (
+            <div id='section-show-project' className="container-show-projects"  >
 
-    const [ projects, setProjects ] = useState([]);
-
-        const getProjects = async( event = null  )=> {
-
-            
-            
-        }
-        
-        useEffect(()=>{ 
-            getProjects(); 
-        },[]);
-
-        return (
-            <div className="container-show-projects"  >
-                { 
-                    projects.map( (projectValue, key)=>{
-                       return <div className='project-container' key={projectValue.id}>
-                                <div id={ `container-img-project${ projectValue.id }` } className={ 'container-img-project show-disabled' }>
-                                    <img className='img-project' src={projectValue.project_file}/>
-                                </div>
-                                <div className='container-data-project'>
-                                    <h4 className='title-project' >{projectValue.name_project}</h4>
-                                    <p className='sub-title-project'>Tecnologias: <span className='technology'>{projectValue.name_technology}</span></p>
-                                    <a className='link' href={'https://'+projectValue.project_link} target='_blank' rel='noopener noreferrer' >Enlace del proyecto</a>
-                                </div>
-                                <div className='icons-container'>
-                                    <button className='icons-projects icon-update' >
-                                        <Link id={ projectValue.id } 
-                                            onClick={ (event)=> fillInFormField( event, projectValue, projectValue.project_file ) } 
-                                            className='link-icon-update' to='panel-control'> 
-                                                p
-                                        </Link>
-                                    </button>
-        
-                                { loginResponse && (
-                                    <button className='icons-projects icon-delete' >
-                                        <Link id={ projectValue.id } 
-                                            onClick={ async( e ) =>  { await projectDelete(e), await getProjects( e ) }} 
-                                            className='link-icon-delete' to='/'>
-                                                ' 
-                                        </Link>
-                                    </button>
-                                )}
-                                    <button id={ projectValue.id } className = 'icons-projects icon-show' 
+                { Array.isArray( projectsState ) && projectsState.map( (projectValue, key)=>{
+                       return (
+                                <div className='project-container' key={projectValue.id}>
+                                    <div id={ `container-img-project${ projectValue.id }` } className={ 'container-img-project show-disabled' }>
+                                        <img className='img-project' src={projectValue.project_file}/>
+                                    </div>
+                                    <div className='container-data-project'>
+                                        <h4 className='title-project' >{projectValue.name_project}</h4>
+                                        <p className='sub-title-project'>Tecnologias: <span className='technology'>{projectValue.name_technology}</span></p>
+                                        <a className='link' href={'https://'+projectValue.project_link} target='_blank' rel='noopener noreferrer' >Enlace del proyecto</a>
+                                    </div>
+                                    <div className='icons-container'>
+                                        <button id={ projectValue.id } className = 'icons-projects icon-show' 
                                             onClick={ changeClass }>
                                                 I
-                                    </button>
-                                    
-                                </div>
-                        </div> 
-                    } )
+                                        </button>
+                                    </div>
+                                </div> 
+                              )
+                    })
                 }
     
             </div>
-      )
-
+    )
 };
