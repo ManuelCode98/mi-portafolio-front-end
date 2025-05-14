20f327d41f10252969d1a5f2b2234c5d6e520a4a
Manuel Rodriguez <cuentapersonal.0098@gmail.com>
2025-05-14 16:17:25 -0500

    .\src\components\header-image-background.jsx: Se agrego el link de descarga del cv

--- src/components/header-image-background.jsx
+++ src/components/header-image-background.jsx
@@ -1,19 +1,17 @@
 
 import headerImageBackground from '../assets/imgs/header-image-background/header-image-background.png';
-
 import linkedinIcon from '../assets/imgs/social-media-icons/linkedin.png';
 import whatsappIcon from '../assets/imgs/social-media-icons/whatsapp.png';
 import instagramIcon from '../assets/imgs/social-media-icons/instagram.png';
 import facebookIcon from '../assets/imgs/social-media-icons/facebook.png';
 import downloadIcon from '../assets/imgs/social-media-icons/download.png';
-
-
-
 import '../assets/style/header-image-background.css';
+
 
 const title = 'Manuel Rodriguez';
 const subTitle = 'Desarrollador de software en Palmira-Colombia';
 const cv = 'Descarga mi CV';
+const linkCv = 'https://drive.usercontent.google.com/download?id=15nalZmKy0MUf6grES5akU5Jtw6CbvNei&export=download&authuser=0&confirm=t&uuid=3253bfa6-338a-4ba5-b8c6-4c877cf3c8a8&at=ALoNOgnKJ_tNI-73a-VH1UD0QY1O:1747256844710';
 
 const linkLinkedin = 'https://www.linkedin.com/in/manuel-rodriguez-ba5b5b339/';
 const linkWhatsapp = 'https://wa.me/3157382433';
@@ -34,7 +32,7 @@ const HeaderImageBackground = ()=>{
                     <a href={ linkInstagram } target='_blank' > <img className='icons instagram-icon' src={instagramIcon} /> </a>
                     <a href={ linkFacebook }  target='_blank' > <img className='icons facebook-icon' src={facebookIcon} /> </a>
                 </span>
-                <button className='cv'><a>{cv}</a>
+                <button className='cv' target='_blank' href={linkCv}><a target='_blank' href={linkCv}>{cv}</a>
                     
                     <div className='icon-download-container'>
                         <img className='icon-download' src={downloadIcon} />
